fa4c36d2d442375ca0fad6c4495d815d7180bd92
Eshan Singh <eshansingh@gmail.com>
2019-05-17 20:46:59 +0530

    Add parked vehicles report
    Along with loading bar in main screen.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,16 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Vehicle Management',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomePage(title: 'Vehicle Management'),
-        '/vin-entry': (context) => VinEntryPage(),
-      }
-    );
+        title: 'Vehicle Management',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomePage(title: 'Vehicle Management'),
+          '/vin-entry': (context) => VinEntryPage(),
+          '/reports': (context) => ReportsPage(),
+        });
   }
 }
 
@@ -34,11 +35,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _showLoadingIndicator = false;
+
   handleActionRequest(action) async {
     final vin = await Navigator.pushNamed(context, '/vin-entry');
 
     if (vin == null) {
-      return null; // Means user pressed back and implicitly cancelled.
+      return -1; // Means user pressed back and implicitly cancelled.
     }
 
     final responseRaw = await http.get(
@@ -59,155 +62,245 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Widget makeActionButton(BuildContext context, String actionName,
+      String actionFriendlyName, Function successMessage, IconData icon) {
+    return RaisedButton(
+      onPressed: () async {
+        try {
+          setState(() {
+            _showLoadingIndicator = true;
+          });
+
+          var result = await handleActionRequest(actionName);
+
+          if (result == -1) {
+            return;
+          }
+
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                content: SingleChildScrollView(
+                  child: Text(successMessage(result)),
+                ),
+              );
+            },
+          );
+        } catch (e) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                content: SingleChildScrollView(
+                  child: Text('Error occured: ${e.message}'),
+                ),
+              );
+            },
+          );
+        } finally {
+          setState(() {
+            _showLoadingIndicator = false;
+          });
+        }
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon, size: IconTheme.of(context).size * 2),
+          Text(actionFriendlyName, style: TextStyle(fontSize: 20.0)),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        // bottom: PreferredSize(
-        //   preferredSize: Size(double.infinity, 1.0),
-        //   child: LinearProgressIndicator(valueColor:
-        //       AlwaysStoppedAnimation<Color>(Colors.orange)),
-        // ),
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15.0),
-          child: ListView(
-            itemExtent: 50.0,
-            padding: EdgeInsets.symmetric(vertical: 15.0),
-            children: <Widget>[
-              RaisedButton.icon(
-                onPressed: () async {
-                  try {
-                    var result = await handleActionRequest('park-in');
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: SingleChildScrollView(
-                            child: Text('Vehicle assigned to slot ${result["slot"]}'),
-                          ),
-                        );
-                      },
-                    );
-                  } catch (e) {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: SingleChildScrollView(
-                            child: Text('Error occured: ${e.message}'),
-                          ),
-                        );
-                      }
-                    );
-                  }
-                },
-                label: Text('Park In', style: TextStyle(fontSize: 20.0)),
-                icon: Icon(Icons.add),
-              ),
-              SizedBox(width: 30.0),
-              RaisedButton.icon(
-                onPressed: () async {
-                  try {
-                    var result = await handleActionRequest('park-out');
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: SingleChildScrollView(
-                            child: Text('Vehicle marked as parked out'),
-                          ),
-                        );
-                      }
-                    );
-                  } catch (e) {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: SingleChildScrollView(
-                            child: Text('Error occured: ${e.message}'),
-                          ),
-                        );
-                      }
-                    );
-                  }
-                },
-                label: Text('Park Out', style: TextStyle(fontSize: 20.0)),
-                icon: Icon(Icons.eject),
-              ),
-              SizedBox(width: 30.0),
-              RaisedButton.icon(
-                onPressed: () async {
-                  try {
-                    var result = await handleActionRequest('maint-in');
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: SingleChildScrollView(
-                            child: Text('Vehicle assigned to ${result["slot"]}'),
-                          ),
-                        );
-                      }
-                    );
-                  } catch (e) {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: SingleChildScrollView(
-                            child: Text('Error occured: ${e.message}'),
-                          ),
-                        );
-                      }
-                    );
-                  }
-                },
-                label: Text('Maintainence In', style: TextStyle(fontSize: 20.0)),
-                icon: Icon(Icons.vertical_align_bottom),
-              ),
-              SizedBox(width: 30.0),
-              RaisedButton.icon(
-                onPressed: () async {
-                  try {
-                    var result = await handleActionRequest('maint-out');
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: SingleChildScrollView(
-                            child: Text('Vehicle marked as out for maintainence'),
-                          ),
-                        );
-                      }
-                    );
-                  } catch (e) {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: SingleChildScrollView(
-                            child: Text('Error occured: ${e.message}'),
-                          ),
-                        );
-                      }
-                    );
-                  }
-                },
-                label: Text('Maintainence Out', style: TextStyle(fontSize: 20.0)),
-                icon: Icon(Icons.vertical_align_top),
-              ),
-              SizedBox(width: 30.0),
-              RaisedButton.icon(
-                onPressed: null,
-                label: Text('Move', style: TextStyle(fontSize: 20.0)),
-                icon: Icon(Icons.open_with),
-              ),
-            ],
+        bottom: PreferredSize(
+          preferredSize: Size(double.infinity, 2.0),
+          child: Visibility(
+            child: LinearProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.orange)),
+            visible: _showLoadingIndicator,
           ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(5.0),
+        child: OrientationBuilder(
+          builder: (context, orientation) {
+            return GridView.count(
+              crossAxisCount: orientation == Orientation.landscape ? 4 : 2,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              children: <Widget>[
+                makeActionButton(
+                    context,
+                    'park-in',
+                    'Park In',
+                    (result) => 'Vehicle assigned to slot ${result["slot"]}',
+                    Icons.add),
+                makeActionButton(context, 'park-out', 'Park Out',
+                    (result) => 'Vehicle marked as parked out', Icons.eject),
+                makeActionButton(
+                    context,
+                    'maint-in',
+                    'Maintainence In',
+                    (result) => 'Vehicle assigned to slot ${result["slot"]}',
+                    Icons.vertical_align_bottom),
+                makeActionButton(
+                    context,
+                    'maint-out',
+                    'Maintainence Out',
+                    (result) => 'Vehicle marked as out for maintainence',
+                    Icons.vertical_align_top),
+                RaisedButton(
+                  onPressed: null,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.open_with,
+                          size: IconTheme.of(context).size * 2),
+                      Text('Move', style: TextStyle(fontSize: 20.0)),
+                    ],
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/reports');
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.assessment,
+                          size: IconTheme.of(context).size * 2),
+                      Text('Reports', style: TextStyle(fontSize: 20.0)),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ReportsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reports'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(5.0),
+        child: OrientationBuilder(
+          builder: (context, orientation) {
+            return GridView.count(
+              crossAxisCount: orientation == Orientation.landscape ? 4 : 2,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        fullscreenDialog: true,
+                        builder: (BuildContext context) {
+                          var vehiclesFuture = http.get(
+                              'https://qi2zwrrpuk.execute-api.us-east-1.amazonaws.com/testing/list-vehicles');
+
+                          return Scaffold(
+                            body: FutureBuilder(
+                              future: vehiclesFuture,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  var vehicles =
+                                      json.decode(snapshot.data.body)["Items"];
+
+                                  var vehiclesInLot = vehicles
+                                      .where((vehicle) =>
+                                          vehicle["in_lot"] as bool)
+                                      .toList();
+
+                                  if (vehiclesInLot.length > 0) {
+                                    return Center(
+                                        child: DataTable(
+                                          rows: vehiclesInLot.map<DataRow>((vehicle) {
+                                            return DataRow(
+                                              cells: <DataCell>[
+                                                DataCell(Text(vehicle["VIN"])),
+                                                DataCell(Text(vehicle["current_slot"]))
+                                              ],
+                                            );
+                                          }).toList(),
+                                          columns: <DataColumn>[
+                                            DataColumn(label: Text('VIN')),
+                                            DataColumn(label: Text('Slot')),
+                                          ],
+                                      ),
+                                    );
+                                  } else {
+                                    return Center(
+                                      child: Text('There are no vehicles in the lot.'),
+                                    );
+                                  }
+                                } else if (snapshot.hasError) {
+                                  debugPrint("Oh no");
+                                }
+
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  child: AutoSizeText('Currently parked vehicles',
+                      style: TextStyle(fontSize: 30.0),
+                      textAlign: TextAlign.center,
+                      maxLines: 3),
+                ),
+                RaisedButton(
+                  onPressed: null,
+                  child: AutoSizeText('Vehicles currently out for maintainence',
+                      style: TextStyle(fontSize: 30.0),
+                      textAlign: TextAlign.center,
+                      maxLines: 3),
+                ),
+                RaisedButton(
+                  onPressed: null,
+                  child: AutoSizeText('Parked out vehicles',
+                      style: TextStyle(fontSize: 30.0),
+                      textAlign: TextAlign.center,
+                      maxLines: 3),
+                ),
+                RaisedButton(
+                  onPressed: null,
+                  child: AutoSizeText('Most recent entries',
+                      style: TextStyle(fontSize: 30.0),
+                      textAlign: TextAlign.center,
+                      maxLines: 3),
+                ),
+                RaisedButton(
+                  onPressed: null,
+                  child: AutoSizeText('Status of all slots',
+                      style: TextStyle(fontSize: 30.0),
+                      textAlign: TextAlign.center,
+                      maxLines: 3),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
